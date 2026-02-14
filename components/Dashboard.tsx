64eafc02d6883d479f73b30fbfbebf5ce12c3a58
aman ge <geaman73@gmail.com>
2026-02-14 20:15:07 +0300

    fix: update location map embed

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -176,9 +176,9 @@ export default function Dashboard() {
             <iframe 
               width="100%" 
               height="100%" 
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1000!2d38.750917!3d9.018472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2set!4v123456789&maptype=satellite"
+              src="https://maps.google.com/maps?q=9.018472,38.750917&z=15&output=embed"
               style={{ border: 0 }}
-              allowFullScreen={false}
+              allowFullScreen
               loading="lazy"
             ></iframe>
           </div>
